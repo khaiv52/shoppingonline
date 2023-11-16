f945266feed45c99491b6ee7ac9c4b8624a3ea46
Nguyen Minh Khai <95909601+khaiv52@users.noreply.github.com>
2023-11-16 14:10:58 +0700

    Add shopping files

---
+++ server/Utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: "clustertest.pim8ki9.mongodb.net",
+    DB_USER: "MinhKhai",
+    DB_PASS: "khai01645",
+    DB_DATABASE: "shoppingonline",
+    JWT_SECRET: "khai01645",
+    JWT_EXPIRES: "86400000", // in milliseconds
+    EMAIL_USER: "minhkhai8252@gmail.com", // gmail service
+    EMAIL_PASS: "irnh rgre waqx oopo",
+};
+module.exports = MyConstants;
