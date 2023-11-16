1f6356ec78f2d5efb51d2d0693c11e0a17cc1e94
Nguyen Minh Khai <95909601+khaiv52@users.noreply.github.com>
2023-11-16 14:46:54 +0700

    add shoppingonline files

---
+++ server/models/AdminDAO.js
@@ -0,0 +1,11 @@
+require('../Utils/MongooseUtil');
+const Models = require("./Models");
+
+const AdminDAO = {
+  async selectByUsernameAndPassword(username, password) {
+    const query = { username: username, password: password };
+    const admin = await Models.Admin.findOne(query);
+    return admin;
+  },
+};
+module.exports = AdminDAO;
